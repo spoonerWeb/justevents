6572fe1fffe090c75cc344c8060afe774166c3a4
Philipp Kitzberger <pk@cron.eu>
2017-04-21 11:48:55 +0000

    Add renderType for type=select fields

--- Configuration/TCA/Event.php
+++ Configuration/TCA/Event.php
@@ -21,6 +21,7 @@ $GLOBALS['TCA']['tx_justevents_domain_model_event'] = array(
 			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
 			'config' => array(
 				'type' => 'select',
+				'renderType' => 'selectSingle',
 				'foreign_table' => 'sys_language',
 				'foreign_table_where' => 'ORDER BY sys_language.title',
 				'items' => array(
@@ -35,6 +36,7 @@ $GLOBALS['TCA']['tx_justevents_domain_model_event'] = array(
 			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.l18n_parent',
 			'config' => array(
 				'type' => 'select',
+				'renderType' => 'selectSingle',
 				'items' => array(
 					array('', 0),
 				),
